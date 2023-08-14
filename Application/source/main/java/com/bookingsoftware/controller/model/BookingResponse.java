bc6fcbf0fe5be0dd17e228830955c0594ed55ed6
Jacob Rebman <jacob.rebman@mykenzie.snhu.edu>
2023-08-14 17:55:22 -0600

    booking object initial creation

--- Application/source/main/java/com/bookingsoftware/controller/model/BookingResponse.java
+++ Application/source/main/java/com/bookingsoftware/controller/model/BookingResponse.java
@@ -1,4 +1,35 @@
 package com.bookingsoftware.controller.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingResponse {
+
+    //
+    @JsonProperty("Activity_Id")
+    private String activityID;
+
+    @JsonProperty("Activity_Name")
+    private String activityName;
+
+    @JsonProperty("Activity_Duration")
+    private String activityDuration;
+
+    @JsonProperty("Activity_Date")
+    private String activityDate;
+
+    @JsonProperty("Activity_Price")
+    private String price;
+
+    @JsonProperty("Activity_Description")
+    private String description;
+
+    @JsonProperty("Customer_Name")
+    private String customer;
+
+    @JsonProperty("CustomerPhoneNumber")
+    private String phoneNumber;
+
 }
